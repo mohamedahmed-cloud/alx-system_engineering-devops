cf107ff3785141eaebe6b34e45a279f6662697ec
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-11 01:23:28 +0300

    Add File 2-while_best_school

--- 0x04-loops_conditions_and_parsing/push.sh
+++ 0x04-loops_conditions_and_parsing/push.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 git add --chmod +x $1
-git add *
-git commit -m "Add File $1"
-git push
+shellcheck $1
+if [ $? -ne 1 ]; then
+
+	git add *
+	git commit -m "Add File $1"
+	git push
+fi
