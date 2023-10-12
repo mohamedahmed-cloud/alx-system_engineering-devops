facf864a70a43c6e265bca04e1775e32226f9a01
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-12 12:59:24 +0300

    Add File 1-list_your_processes

--- 0x05-processes_and_signals/push.sh
+++ 0x05-processes_and_signals/push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 while true; do
 	read -p "Enter File Name To run script: " file
-	# git add --chmod +x $file
-	# shellcheck $file
+	git add --chmod +x $file
+	shellcheck $file
 	if [ $? -ne 1 ]; then
 
 		git add .
