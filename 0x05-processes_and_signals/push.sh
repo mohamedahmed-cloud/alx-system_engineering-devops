14087aac8eb370fe4ca15ee508f94b9cc6cd775d
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-12 12:52:54 +0300

    Add File 0-what-is-my-pid

--- 0x05-processes_and_signals/push.sh
+++ 0x05-processes_and_signals/push.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 while true; do
-	read -p "Enter Anything To run: " file
-	git add --chmod +x $1
-	shellcheck $1
+	read -p "Enter File Name To run script: " file
+	git add --chmod +x $file
+	shellcheck $file
 	if [ $? -ne 1 ]; then
 
 		git add .
-		git commit -m "Add File $1"
+		git commit -m "Add File $file"
 		git push
 	fi
 done
