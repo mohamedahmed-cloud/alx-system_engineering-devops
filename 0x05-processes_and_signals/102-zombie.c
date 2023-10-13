5b19da087d160a374b5bddaba49b3e110ebca2fe
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-13 21:41:55 +0300

    add zoombie task

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -8,15 +8,18 @@
 int main(void)
 {
 	int cnt = 0;
+	int x = 0;
 
 	while (cnt < 5)
 	{
 		if (fork() == 0)
 		{
 			printf("Zombie process created, PID: %d\n", getpid());
-
-		}
 			cnt++;
+			exit(0)
+		}
+		if (x == 20) break;
+		x++;
 	}
 	return (0);
 }
