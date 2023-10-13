d5ffcbea62d0c2d9e79b7c8109062d360d477273
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-13 21:38:55 +0300

    add zoombie task

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include "unistd.h"
+#include <stdlib.h>
+/**
+ * main - The Main function.
+ * Return: always return 0
+*/
+int main(void)
+{
+	int cnt = 0;
+
+	while (cnt < 5)
+	{
+		if (fork() == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+
+		}
+			cnt++;
+	}
+	return (0);
+}
