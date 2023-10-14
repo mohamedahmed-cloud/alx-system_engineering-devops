4a879611e1f4e835753791b6f327563a9bf98c00
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-14 03:04:43 +0300

    Add File 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -3,10 +3,5 @@
 # The regular expression must match School
 # Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular expression matching method
 
-string_to_match = ARGV[0]
-regux_check = /\bSchool\b/
-is_match = regux_check.match(string_to_match)
-if is_match
-	puts "School"
-end
 
+puts ARGV[0].scan(/School/).join
