4c89884358cc40c509187f8b83eb6c6a4412750d
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-14 17:46:59 +0300

    Add File 4-repetition_token_3.rb

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# Requirements:
+# The regular expression must be exactly matching a string that 
+# starts with h ends with n and can have any single character in between
+# Using the project instructions, create a Ruby script that accepts 
+# one argument and pass it to a regular expression matching method
+puts ARGV[0].scan("/h[a-zA-Z]*n/")
