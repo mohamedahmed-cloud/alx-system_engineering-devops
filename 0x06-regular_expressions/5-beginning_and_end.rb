a015078c4b0942bca5ef602db5c8caa3db2ecf79
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-14 17:50:22 +0300

    Add File 5-beginning_and_end.rb

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -4,4 +4,4 @@
 # starts with h ends with n and can have any single character in between
 # Using the project instructions, create a Ruby script that accepts 
 # one argument and pass it to a regular expression matching method
-puts ARGV[0].scan(/^h[a-zA-Z]*n$/).join
+puts ARGV[0].scan(/^h\D*\d*n$/).join
